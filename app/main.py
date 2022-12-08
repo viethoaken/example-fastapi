2f8cd3e53cfc219e77749589abb072aaca8a2724
hoadong <viethoaken@gmail.com>
2022-12-08 14:21:54 +0700

    deploy to heroku

--- app/main.py
+++ app/main.py
@@ -36,7 +36,7 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to FastAPI with bind mount in docker compose"}
+    return {"message": "Welcome to Heroku deployed from use CI/CD pipeline"}
 
 # my_posts = [{'title': 'title 1', 'content': 'content 1', 'id': 1}, {'title': 'title 2', 'content': 'content 2', 'id': 2}]
 
